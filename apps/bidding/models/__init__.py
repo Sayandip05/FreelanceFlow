62767770c884e0d4034d86d4364f046388193cad
Sayandip Bar <sayandip@Sayandips-MacBook-Neo.local>
2026-06-22 23:46:17 +0530

    refactor(bidding): move models to package

---
+++ apps/bidding/models/__init__.py
@@ -0,0 +1,5 @@
+from .models import Bid, Contract
+from .models_amendment import ContractAmendment
+from .models_extended import WorklogApproval, BidRetraction, CounterOffer
+from .models_review import Review, ReviewResponse
+from .models_termination import ContractTerminationRequest
