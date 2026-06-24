a1a795d88f00e6c400516a364ba4301d93319e1e
Sayandip Bar <sayandipbar05@gmail.com>
2026-06-25 00:20:32 +0530

    refactor(payments): move serializers to package

---
+++ apps/payments/serializers/__init__.py
@@ -0,0 +1,14 @@
+from .serializers import (
+    EscrowSerializer,
+    PaymentSerializer,
+    PaymentListSerializer,
+    CreateEscrowSerializer,
+    ReleasePaymentSerializer,
+    PlatformEarningSerializer,
+    PaymentHistorySerializer,
+)
+from .serializers_extended import (
+    PaymentMilestoneSerializer,
+    CreateMilestoneSerializer,
+    MilestoneProgressSerializer,
+)
