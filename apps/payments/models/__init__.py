35313d1b97530b9bd9e1014626480ebc9815f462
Sayandip Bar <sayandipbar05@gmail.com>
2026-06-25 00:20:26 +0530

    refactor(payments): move models to package

---
+++ apps/payments/models/__init__.py
@@ -0,0 +1,4 @@
+from .models import Payment, Escrow, PlatformEarning, PaymentEvent
+from .models_dispute import PaymentDispute, DisputeMessage
+from .models_extended import TaxDocument, CurrencyExchangeRate, MultiCurrencyPayment
+from .models_milestone import PaymentMilestone
