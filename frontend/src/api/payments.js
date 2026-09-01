b1d41819b8ebcc1c332238705551e35b058e5865
Debabrata Dey <debabratadey8080@gmail.com>
2026-09-01 19:56:36 +0530

    Fix authorixation issue in frontend

--- frontend/src/api/payments.js
+++ frontend/src/api/payments.js
@@ -71,8 +71,10 @@ export const paymentsAPI = {
     api.post(`/payments/milestones/${milestoneId}/fund-from-wallet/`),
   
   // Invoice / Receipt Download url helper
-  getReceiptDownloadUrl: (txId, type = 'payment') => 
-    `${api.defaults.baseURL || ''}/payments/transactions/${txId}/receipt/?type=${type}`
+  getReceiptDownloadUrl: (txId, type = 'payment') => {
+    const token = localStorage.getItem('access_token');
+    return `${api.defaults.baseURL || ''}/payments/transactions/${txId}/receipt/?type=${type}&access=${token}`;
+  }
 }
 
 export default paymentsAPI
